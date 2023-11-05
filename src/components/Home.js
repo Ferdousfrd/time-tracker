626311490f408d14ba698e35647422489f507c8e
Ferdousfrd <77578937+Ferdousfrd@users.noreply.github.com>
2023-11-05 23:36:05 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <div>
       <h2>Welcome to the Activity Tracker App!</h2>
+      <h3>School Project By md Fakhruddin Ferdous</h3>
+      <h3>Full Stack Web(frontEnd)</h3>
+      <h5>About & Insructions:</h5>
       <p>
         This app allows you to record and track your daily activities. Whether it's work, leisure, or any other task,
         you can keep a log of how you spend your time.

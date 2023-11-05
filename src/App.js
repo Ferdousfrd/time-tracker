6ee25f4587c4cab25d4b53f562dac4ad70d9684d
ferdous_7272 <ferdous.frd81@gmail.com>
2023-11-05 23:17:16 +0200

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -9,42 +9,48 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentView: 'home',
-      activities: [],
+      currentView: 'home', // Initialize the current view as 'home'
+      activities: [], // Initialize the activities array to store recorded activities
     };
   }
 
+  // Function to handle recording activities
   handleActivityRecording = (activity) => {
-    const activities = [...this.state.activities, activity];
-    this.setState({ activities });
+    const activities = [...this.state.activities, activity]; // Create a new array with the recorded activity
+    this.setState({ activities }); // Update the state with the new activities
   };
 
+  // Function to change the current view
   changeView = (view) => {
-    this.setState({ currentView: view });
+    this.setState({ currentView: view }); // Set the current view to the specified view
   };
 
+  // Function to delete an activity by index
   onDeleteActivity = (index) => {
     const activities = [...this.state.activities];
-    activities.splice(index, 1);
-    this.setState({ activities });
+    activities.splice(index, 1); // Remove the activity at the specified index
+    this.setState({ activities }); // Update the state without the deleted activity
   };
 
   render() {
     return (
       <div className="App">
         <nav>
-          <button onClick={() => this.changeView('home')}>Home</button>
-          <button onClick={() => this.changeView('activityTracking')}>Activity Tracking</button>
-          <button onClick={() => this.changeView('reports')}>Reports</button>
+          <button onClick={() => this.changeView('home')}>Home</button> {/* Button to switch to the 'Home' view */}
+          <button onClick={() => this.changeView('activityTracking')}>Activity Tracking</button> {/* Button to switch to the 'Activity Tracking' view */}
+          <button onClick={() => this.changeView('reports')}>Reports</button> {/* Button to switch to the 'Reports' view */}
         </nav>
-        {this.state.currentView === 'home' && <Home />}
+        {this.state.currentView === 'home' && <Home />} {/* Render the Home component if the current view is 'home' */}
         {this.state.currentView === 'activityTracking' && (
           <ActivityTracking
             onActivityRecorded={this.handleActivityRecording}
             activities={this.state.activities}
+            onDeleteActivity={this.onDeleteActivity} // Pass the onDeleteActivity function to the ActivityTracking component
           />
         )}
-        {this.state.currentView === 'reports' && <Reports activities={this.state.activities} />}
+        {this.state.currentView === 'reports' && (
+          <Reports activities={this.state.activities} />
+        )}
         {this.state.currentView === 'activityTracking' && (
           <ActivityList activities={this.state.activities} onDeleteActivity={this.onDeleteActivity} />
         )}
--- src/components/ActivityTracking.js
+++ src/components/ActivityTracking.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ActivityForm from './ActivityForm';
-import ActivityList from './ActivityList';
+
 
 const ActivityTracking = ({ onActivityRecorded, activities }) => {
   return (
     <div>
       <h2>Activity Tracking</h2>
       <ActivityForm onActivityRecorded={onActivityRecorded} />
-      <ActivityList activities={activities} />
+      <h3>Recorded Activities</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Activity</th>
+            <th>Start Time</th>
+            <th>End Time</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {activities.map((activity, index) => (
+            <tr key={index}>
+              <td>{activity.name}</td>
+              <td>{activity.startTime.toLocaleTimeString()}</td>
+              <td>{activity.endTime ? activity.endTime.toLocaleTimeString() : 'Recording...'}</td>
+              <td>
+                <button onClick={() => onActivityRecorded(activity)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
